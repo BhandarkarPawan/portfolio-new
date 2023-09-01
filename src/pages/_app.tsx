6797cce3cd69e47b3630f2ddd0ac1aab7f8a60bb
BhandarkarPawan <pawankbhandarkar@gmail.com>
2023-09-01 00:43:00 -0700

    Add Helmet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,15 @@ import DARK_THEME from "theme";
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={DARK_THEME}>
+            {/* Basic OG tags */}
+            <meta property="og:title" content="Portfolio" />
+            <meta
+                property="og:description"
+                content="I’m a full-stack engineer specializing in building (and sometimes designing) highly accessible, human-centered applications for the web."
+            />
+            <meta property="og:image" content="/images/link-preview.png" />
+            <meta property="og:url" content="https://bhandarkar.me/" />
+
             <ResetStyles />
             <GlobalStyles />
             <Component {...pageProps} />
