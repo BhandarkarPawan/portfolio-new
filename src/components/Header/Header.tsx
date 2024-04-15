e709a03e18b12d47e32e70fd7853a9e11d72d3c7
BhandarkarPawan <pawankbhandarkar@gmail.com>
2024-04-14 23:10:18 -0700

    Header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import Navigation from "@/components/Navigation";
-import { QUERIES } from "breakpoints";
 import { Squash as Hamburger } from "hamburger-react";
 import { useState } from "react";
-import styled from "styled-components";
 import { SocialIcons } from "../Socials/Socials";
+import styles from "./Header.module.css";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = ({
   scrollDirection,
@@ -23,21 +25,28 @@ const Header = ({
     theme === "light" ? "/images/logo-light.png" : "/images/logo.png";
   return (
     <>
-      <Overlay show={showSidebar} onClick={toggleSidebar}>
-        <Sidebar show={showSidebar}>
-          <NavigationWrapper>
+      <div
+        className={clsx(styles.overlay, showSidebar && styles.show)}
+        onClick={toggleSidebar}
+      >
+        <aside className={clsx(styles.sidebar, showSidebar && styles.show)}>
+          <div className={styles.navigationWrapper}>
             <Navigation />
-          </NavigationWrapper>
-          <IconWrapper>
+          </div>
+          <div className={styles.iconWrapper}>
             <SocialIcons />
-          </IconWrapper>
-        </Sidebar>
-      </Overlay>
-      <Wrapper scrollDirection={scrollDirection}>
-        {/* <button onClick={toggleTheme}>Toggle Theme</button> */}
-
-        <Logo src={logoUrl} alt="Logo" />
-        <Navbar>
+          </div>
+        </aside>
+      </div>
+      <div className={clsx(styles.wrapper, styles[scrollDirection || ""])}>
+        <Image
+          className={styles.logo}
+          width={43}
+          height={41}
+          src={logoUrl}
+          alt="Logo"
+        />
+        <nav className={styles.navbar}>
           <Navigation />
           {/* <DarkModeSwitch
             style={{ marginLeft: "2em" }}
@@ -45,9 +54,9 @@ const Header = ({
             onChange={toggleTheme}
             size={24}
           /> */}
-        </Navbar>
+        </nav>
 
-        <MenuButton>
+        <div className={styles.menuButton}>
           {/* <DarkModeSwitch
             style={{
               marginLeft: "auto",
@@ -59,156 +68,20 @@ const Header = ({
             size={32}
           /> */}
           <Hamburger toggled={showSidebar} onToggle={toggleSidebar} />
-        </MenuButton>
-      </Wrapper>
-      <Banner>
+        </div>
+      </div>
+      <div className={styles.banner}>
         I&apos;m currently building something awesome at{" "}
-        <A href="https://www.convergelab.ai/">Convergelab.ai</A>
-      </Banner>
+        <Link
+          className={styles.aLink}
+          target="_blank"
+          href="https://www.convergelab.ai/"
+        >
+          Convergelab.ai
+        </Link>
+      </div>
     </>
   );
 };
-
-const A = styled.a`
-  text-decoration: underline;
-  color: ${({ theme }) => theme.colors.background.dark};
-`;
-
-const NavigationWrapper = styled.div`
-  margin-top: auto;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  gap: 32px;
-`;
-
-const IconWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: auto;
-  font-size: 24px;
-`;
-
-const Logo = styled.img`
-  width: 43px;
-  height: 41px;
-  flex-shrink: 0;
-
-  ${QUERIES.tabletAndUp} {
-    width: 55px;
-    height: 52px;
-  }
-`;
-
-interface IStyledProps {
-  scrollDirection: "up" | "down" | undefined;
-}
-
-const Wrapper = styled.div<IStyledProps>`
-  position: sticky;
-  top: 0;
-  background-color: ${({ theme }) => theme.colors.background.blur};
-  backdrop-filter: blur(10px);
-  z-index: 999;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  grid-area: header;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  transition: transform 0.3s ease-in-out;
-
-  padding: 8px 16px;
-
-  transform: translateY(
-    ${({ scrollDirection }) => (scrollDirection === "down" ? "-100%" : "0")}
-  );
-
-  ${QUERIES.tabletAndUp} {
-    padding: 8px 32px;
-  }
-
-  ${QUERIES.desktopAndUp} {
-    padding-left: 64px;
-    padding-right: 64px;
-  }
-`;
-
-const Navbar = styled.nav`
-  display: none;
-  align-items: center;
-
-  ${QUERIES.desktopAndUp} {
-    gap: 64px;
-  }
-
-  ${QUERIES.tabletAndUp} {
-    gap: 36px;
-    display: flex;
-  }
-`;
-
-const Banner = styled.div`
-  grid-area: banner;
-  background-color: ${({ theme }) => theme.colors.primary};
-  color: ${({ theme }) => theme.colors.background.regular};
-  padding: 8px 16px;
-  text-align: center;
-  font-size: 14px;
-
-  ${QUERIES.tabletAndUp} {
-    padding: 8px 32px;
-  }
-`;
-
-const Sidebar = styled.nav<{ show: boolean }>`
-  z-index: 999;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  gap: 32px;
-
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: 80%;
-
-  background-color: ${({ theme }) => theme.colors.background.regular};
-  padding: 32px;
-
-  transform: translateX(100%);
-  transition: transform 0.3s ease-in-out;
-
-  ${({ show }) => show && "transform: translateX(0);"}
-
-  ${QUERIES.tabletAndUp} {
-    display: none;
-  }
-`;
-
-const MenuButton = styled.button`
-  display: flex;
-
-  ${QUERIES.tabletAndUp} {
-    display: none;
-  }
-`;
-
-const Overlay = styled.div<{ show: boolean }>`
-  z-index: 1000;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  background-color: rgba(0, 0, 0, 0.5);
-  opacity: 0;
-  transition: opacity 0.3s ease-in-out;
-  pointer-events: none;
-
-  ${({ show }) => show && "opacity: 1;"}
-  ${({ show }) => show && "pointer-events: all;"}
-`;
 
 export default Header;
